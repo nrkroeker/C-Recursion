ac81072d9abd87e85e89747e1ee8f549b1f658f6
nrkroeker <nathaliekroeker@gmail.com>
2017-01-17 15:36:52 -0500

    setting up files

---
+++ textWizard.c
@@ -0,0 +1,5 @@
+#include <stdio.h> #include <string.h>
+// This function uses recursion to reverse a word in C. void reversedWord (char input [], int size) {
+}
+// This is our main method that is responsible for running our program. int main () {
+}
